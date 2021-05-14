610c89a51342dcb191d6ccc9cacd539c07c1a839
Witold Wójciak <witold.wojciak@gmail.com>
2021-05-14 23:26:48 +0200

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+console.log("Cześć! Ciesze się, że sprawdzasz mój kod, daj znać jak coś nie zostało zrobione dobrze :)")
+
+let button = document.querySelector(".js-body__button");
+let body = document.querySelector(".body");
+let themeNameColor = document.querySelector(".body__themeNameColor");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    themeNameColor.innerText = body.classList.contains(".dark") ? "niebieskie" : "ciemne";
+});
+
+
+
+
